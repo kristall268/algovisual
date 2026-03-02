34cb9acabd8340a8b01c552ab572fbecc0d69f87
kristall268 <kristall268@outlook.com>
2026-03-02 12:37:17 +0500

    check

--- main.cpp
+++ main.cpp
@@ -1,5 +1,31 @@
-#include "imgui/imgui.h"
+#include "imgui_impl_vulkan.h"
 #include <iostream>
-
 using namespace std;
-int main() { cout << IMGUI_CHECKVERSION() << endl; }
+
+#ifdef IMGUI_IMPL_VULKAN_USE_VOLK
+#define VOLK_IMPLEMENTATIONS
+#include <volk.h>
+#endif
+
+static VkAllocationCallbacks *g_Allocator = nullptr;
+static VkInstance g_Instance = VK_NULL_HANDLE;
+static VkPhysicalDevice g_PhysicalDevice = VK_NULL_HANDLE;
+static VkDevice g_Device = VK_NULL_HANDLE;
+static uint32_t g_QueueFamily = (uint32_t)-1;
+static VkQueue g_Queue = VK_NULL_HANDLE;
+static VkPipelineCache g_PipelineCache = VK_NULL_HANDLE;
+static VkDescriptorPool g_DescriptorPool = VK_NULL_HANDLE;
+
+static ImGui_ImplVulkanH_Window g_MainWindowData;
+static uint32_t g_MinImageCount = 2;
+static bool g_SwapChainRebuild = false;
+
+static void checK_vk_result(VkResult err) {
+  if (err == 0) {
+    return;
+  }
+  cout << "[vulkan] Error: VkResult = " << err << endl;
+  if (err < 0) {
+    abort();
+  }
+}
