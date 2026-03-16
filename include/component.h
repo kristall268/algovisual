bcad75abcbd2bfe9d39db1bd0975990c6d94e771
kristall268 <kristall268@outlook.com>
2026-03-16 20:04:47 +0500

    reconfiguration

---
+++ include/component.h
@@ -0,0 +1,18 @@
+#include "imgui_impl_vulkan.h"
+#include <vulkan/vulkan_core.h>
+
+// Data
+static VkAllocationCallbacks *g_Allocator = nullptr;
+static VkInstance g_Instance = VK_NULL_HANDLE;
+static VkPhysicalDevice g_PhysicalDevice = VK_NULL_HANDLE;
+static VkDevice g_Device = VK_NULL_HANDLE;
+static uint32_t g_QueueFamily = (uint32_t)-1;
+static VkQueue g_Queue = VK_NULL_HANDLE;
+static VkPipelineCache g_PipelineCache = VK_NULL_HANDLE;
+static VkDescriptorPool g_DescriptorPool = VK_NULL_HANDLE;
+
+static ImGui_ImplVulkanH_Window g_MainWindowData;
+static uint32_t g_MinImageCount = 2;
+static bool g_SwapChainRebuild = false;
+
+static void check_vk_result(VkResult err);
